c6a052358dd55cb4b9c7aafac770a9413f29371c
Daria <chercasova_dasha@mail.ru>
2023-05-18 21:05:51 +0700

    Edit pages for product

--- BookClub/Windows/StartWindow.xaml.cs
+++ BookClub/Windows/StartWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Windows;
 using BookClub.Context;
 using BookClub.Pages;
 
@@ -7,12 +8,13 @@ namespace BookClub
 {
     public partial class StartWindow : Window
     {
+        public static string TitleText = "";
         public StartWindow()
         {
             InitializeComponent();
             FrameClass.GeneralFrame = MainFrame;
         }
-
+        #region Обработчики события Button_Click
         private void AuthoButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Находится в разработке!", 
@@ -26,12 +28,25 @@ namespace BookClub
         /// </summary>
         private void GuestButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Добро пожаловать!",
-                "Книжный клуб",
-                MessageBoxButton.OK,
-                MessageBoxImage.Information);
-            MainStackPanel.Visibility = Visibility.Collapsed;
-            FrameClass.GeneralFrame.Navigate(new ProductPage());
+            try
+            {
+                MessageBox.Show("Добро пожаловать!",
+                    "Книжный клуб",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                MainStackPanel.Visibility = Visibility.Collapsed;
+                FrameClass.GeneralFrame.Navigate(new ProductPage());
+                TitlePageLabel.Content = TitleText;
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка в работе приложения",
+                    "Книжный клуб",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+           
         }
+        #endregion
     }
 }
