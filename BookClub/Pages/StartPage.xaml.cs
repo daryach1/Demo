170d3d7e5a93ac43f8e401d6a9bc9ecbd179c8e0
(MVI) Владимир Иванович Моисеев <mvi@KHPK.RU>
2023-05-19 10:15:35 +0700

    Correct forms

---
+++ BookClub/Pages/StartPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using BookClub.Context;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace BookClub.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Добро пожаловать!",
+                    "Книжный клуб",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+                FrameClass.GeneralFrame.Navigate(new ProductPage());
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка в работе приложения",
+                    "Книжный клуб",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void AuthoButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Находится в разработке!",
+                    "Книжный клуб",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+        }
+    }
+}
--- BookClub/Windows/StartWindow.xaml.cs
+++ BookClub/Windows/StartWindow.xaml.cs
@@ -13,39 +13,28 @@ namespace BookClub
         {
             InitializeComponent();
             FrameClass.GeneralFrame = MainFrame;
+            FrameClass.GeneralFrame.Navigate(new StartPage());
         }
         #region Обработчики события Button_Click
-        private void AuthoButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Находится в разработке!", 
-                "Сообщение пользователю", 
-                MessageBoxButton.OK, 
-                MessageBoxImage.Information);
-        }
 
-        /// <summary>
-        /// Для перехода к списку товаров как гость.
-        /// </summary>
-        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (MainFrame.CanGoBack)
             {
-                MessageBox.Show("Добро пожаловать!",
-                    "Книжный клуб",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Information);
-                MainStackPanel.Visibility = Visibility.Collapsed;
-                FrameClass.GeneralFrame.Navigate(new ProductPage());
-                TitlePageLabel.Content = TitleText;
+                MainFrame.GoBack();
+                TitleText = "";
             }
-            catch (System.Exception)
-            {
-                MessageBox.Show("Ошибка в работе приложения",
-                    "Книжный клуб",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-            }
-           
+                
+        }
+        #endregion
+
+        #region Обработчик MainFrame для BackButton
+        private void MainFrame_ContentRendered(object sender, System.EventArgs e)
+        {
+            if (!MainFrame.CanGoBack)
+                BackButton.Visibility = Visibility.Collapsed;
+            else BackButton.Visibility = Visibility.Visible;
+            TitlePageLabel.Content = TitleText;
         }
         #endregion
     }
