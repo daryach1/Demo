2b0f62486eb55966fca78a8cead1a98dd2a98e33
(MVI) Владимир Иванович Моисеев <mvi@KHPK.RU>
2023-05-19 13:14:43 +0700

    View order

--- BookClub/Pages/ProductPage.xaml.cs
+++ BookClub/Pages/ProductPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using BookClub.Context;
+using BookClub.Windows;
 
 
 namespace BookClub.Pages
@@ -8,15 +13,53 @@ namespace BookClub.Pages
     /// </summary>
     public partial class ProductPage : Page
     {
+        BookClubEntities Context;
         public ProductPage()
         {
             InitializeComponent();
+            ViewOrderButton.IsEnabled = false;
             StartWindow.TitleText = Title.ToString();
+            BaseWork();
         }
-
-        private void GenerOrderButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        #region Обработчики событий
+        private void GenerOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var CurrentProduct = (Products)ProductDataGrid.SelectedItem;
+                ViewOrderWindow.ProductForOrder = CurrentProduct;
+                ViewOrderButton.IsEnabled = true;
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Ошибка в работе приложения",
+                    "Книжный клуб",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+        private void ViewOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewOrderWindow ViewOrderWindow = new ViewOrderWindow();
+            ViewOrderWindow.ShowDialog();
+        }
+        private void AddOrderMenuItem_Click(object sender, RoutedEventArgs e)
         {
 
         }
+        #endregion
+
+        #region Различные методы для работы
+
+        /// <summary>
+        /// Метод для присваивания значений из базы данных
+        /// </summary>
+        private void BaseWork()
+        {
+            Context = new BookClubEntities();
+            ProductDataGrid.ItemsSource = Context.Products.ToList();
+        }
+
+        #endregion
     }
 }
---
+++ BookClub/Windows/ViewOrderWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using BookClub.Context;
+using System.Windows;
+
+
+namespace BookClub.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewOrderWindow.xaml
+    /// </summary>
+    public partial class ViewOrderWindow : Window
+    {
+        public static Products ProductForOrder;
+        public ViewOrderWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
